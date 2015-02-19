315c27ea352fe6aa0135ed2afeecfa62f9ee3398
Jason McTaggart <Jason.mctaggart@icloud.com>
2015-02-19 12:46:44 -0500

    Revert "Login/registration page fields use ng-hide instead"
    This reverts commit 5f0b0fcdb9bec547da4c345872f0e63b6e19f198.

--- src/Login/LoginController.ts
+++ src/Login/LoginController.ts
@@ -3,14 +3,16 @@ module App.Login {
 
     interface ILoginControllerShell extends ng.IScope{
         message: string;
-        login: (data:any) => any;
-        register: (data:any) => any;
-        loginMode: boolean;
+        login: any;
         changeView: any;
-        info: {
+        credentials: {
             email: string
+            password: string
+        };
+        register: {
             firstName: string
             lastName: string
+            email: string
             password: string
             password2: string
         };
@@ -23,26 +25,13 @@ module App.Login {
         public static $inject = ["$scope"];
         constructor ($scope: ILoginControllerShell) {
 
-            //$scope.info = {};
+            $scope.credentials = {email:"", password:""};
             $scope.message="Hello Login Page!!";
 
-            $scope.login =  this.login;
-            $scope.register = (data: any) => {
-                if ($scope.loginMode)
-                    $scope.loginMode = false;
-                else
-                    $scope.loginMode = true;
-            }
-            $scope.loginMode =  this.loginMode;
+            $scope.login =  function(data){
+                console.log(data);
+            };
         }
-
-        loginMode = true;
-
-        login = (data: any) => {
-            console.log(data);
-        }
-
-        
     }
 
     angular.module(LoginController.moduleId, [Nav.NavService.moduleId]).
