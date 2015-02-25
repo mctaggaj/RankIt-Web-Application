5f63f2ab17fc6e9b42088779f8011d774bc7f477
Jason McTaggart <Jason.mctaggart@icloud.com>
2015-02-25 14:42:13 -0500

    Feat(TypeScript): Add typings

--- Typings/RankIt.d.ts
+++ Typings/RankIt.d.ts
@@ -1,13 +1,60 @@
 declare module RankIt {
-    export interface ICompetition {
-        competitionID: any;
+
+    export interface IId extends Number{}
+
+    export interface ILocation extends String{}
+
+    export interface IBase {
         name: string;
-        subject: string;
-        description: string;
-        location: string;
-        public: boolean;
-        results: any[];
-        state: string;
+        description?: string;
+        location: ILocation;
+        state: String;
         streamUrl?:string;
+        participants: IParticipant [];
+        results: IId[];
+
+    }
+
+    export interface ICompetition extends IBase{
+        competitionId: any;
+        subject: string;
+        public: boolean;
+        /**
+         * Now required
+         */
+        description: string;
+        stages: IStage[];
+    }
+
+    export interface IStage extends IBase {
+        stageId: IId;
+        competitionId: IId;
+        previousStage?: IId;
+        nextStage?: IId;
+        seed: number[];
+        events: IEvent[];
+    }
+
+    export interface IEvent extends IBase {
+        eventId: IId;
+        stageId: IId;
+        results: IId[];
+        seed: number[];
+    }
+
+    export interface IParticipant {
+
+        userId: IId;
+        eventId: IId;
+        eventRoleId: IId;
+        rank: number;
+        permissions: IPermissions
+    }
+
+    export interface IPermissions {
+
+        admin: boolean;
+        judge: boolean;
+        competitor: boolean;
     }
 }
