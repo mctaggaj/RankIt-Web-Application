5a8553d4b3d560bfe88abc463db64ffc82c4e3fc
Andrew Welton <awelton@mail.uoguelph.ca>
2015-03-30 10:16:49 -0400

    Small UI Tweak to View competition pages.

--- src/Comp/CompController.ts
+++ src/Comp/CompController.ts
@@ -35,21 +35,26 @@ module App.Comp {
 
         private populateUsers = () => {
             var userList=this.$scope.comp.participants;
-            for(var i=0;i<userList.length;i++){
-                this.dataService.getUser(userList[i].userId).then((data:RankIt.IUser) => {
-                    var temp:any={};
-                    temp.userObject=data;
-                    if(this.baseHelper.userCanEdit(data.userId,this.$scope.comp)){
-                        temp.role="Admin";
-                    }else if(this.baseHelper.userIsCompetitor(data.userId,this.$scope.comp)){
-                        temp.role="Competitor";
-                    }else if(this.baseHelper.userIsJudge(data.userId,this.$scope.comp)){
-                        temp.role="Judge";
-                    }
-                    this.$scope.users.push(temp);
-                }, (failure:any) => {
+            if(userList.length>0){
+                for(var i=0;i<userList.length;i++){
+                    this.dataService.getUser(userList[i].userId).then((data:RankIt.IUser) => {
+                        var temp:any={};
+                        temp.userObject=data;
+                        temp.role="";
+                        if(this.baseHelper.userCanEdit(data.userId,this.$scope.comp)){
+                            temp.role="Admin";
+                        }
+                        if(this.baseHelper.userIsCompetitor(data.userId,this.$scope.comp)){
+                            temp.role.length>0 ? temp.role+=" / Competitor" : temp.role="Competitor";
+                        }
+                        if(this.baseHelper.userIsJudge(data.userId,this.$scope.comp)){
+                            temp.role.length>0 ? temp.role+=" / Judge" : temp.role="Judge";
+                        }
+                        this.$scope.users.push(temp);
+                    }, (failure:any) => {
 
-                });
+                    });
+                }
             }
         }
     }
