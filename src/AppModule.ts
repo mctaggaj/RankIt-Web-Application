81d0207537b95a88ef3262b4d0a5774f20632745
Jason McTaggart <mctaggaj@mail.uoguelph.ca>
2015-04-01 13:01:10 -0400

    fix

--- src/AppModule.ts
+++ src/AppModule.ts
@@ -1,9 +1,9 @@
 /// <reference path="AppGlobals.ts"/>
 /// <reference path="Id/IdModule.ts"/>
-/// <reference path="Base/BaseModule.ts"/>
-/// <reference path="Nav/NavModule.ts"/>
 /// <reference path="Auth/AuthModule.ts"/>
 /// <reference path="Data/DataModule.ts"/>
+/// <reference path="Base/BaseModule.ts"/>
+/// <reference path="Nav/NavModule.ts"/>
 /// <reference path="Shell/ShellModule.ts"/>
 /// <reference path="Home/HomeModule.ts"/>
 /// <reference path="Login/LoginModule.ts"/>
